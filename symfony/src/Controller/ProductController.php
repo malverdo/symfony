0fc2c71cc3cf87326accfcc0e93702ff33e78f30
malverdo <malverdo11@mail.ru>
2022-02-23 10:00:11 +0300

    add ProductController

---
+++ symfony/src/Controller/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController
+{
+
+}
