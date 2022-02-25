e2b07d5cd4bae9389bfc7a8b1bf87296503f3e19
malverdo <malverdo11@mail.ru>
2022-02-25 08:15:43 +0300

    add LuckyController method number

--- symfony/src/Infrastructure/Controller/LuckyController.php
+++ symfony/src/Infrastructure/Controller/LuckyController.php
@@ -6,5 +6,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+    /**
+     * @throws \Exception
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
 
+        return new Response(
+            "<html><body>Lucky number: " . $number . "</body></html>"
+        );
+    }
 }
