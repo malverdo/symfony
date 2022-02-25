5246732f176e213f23f7e2fd5c2a1027022ffd47
malverdo <malverdo11@mail.ru>
2022-02-25 08:28:53 +0300

    add routers POST

--- symfony/src/Infrastructure/Controller/LuckyController.php
+++ symfony/src/Infrastructure/Controller/LuckyController.php
@@ -2,9 +2,10 @@
 
 namespace App\Infrastructure\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
      * @throws \Exception
