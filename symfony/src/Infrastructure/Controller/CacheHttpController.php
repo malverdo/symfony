fb45bb7d9b509612120f9512f9f4b2cc7e75c6b7
malverdo <malverdo11@mail.ru>
2022-02-26 13:14:30 +0300

    add class CacheHttpController index

--- symfony/src/Infrastructure/Controller/CacheHttpController.php
+++ symfony/src/Infrastructure/Controller/CacheHttpController.php
@@ -2,7 +2,22 @@
 
 namespace App\Infrastructure\Controller;
 
-class CacheHttpController
-{
+use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class CacheHttpController  extends AbstractController
+{
+    public function index()
+    {
+        // somehow create a Response object, like by rendering a template
+        $response = $this->render('blog/index.html.twig', []);
+
+        // cache publicly for 3600 seconds
+        $response->setPublic();
+        $response->setMaxAge(3600);
+
+        // (optional) set a custom Cache-Control directive
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+
+        return $response;
+    }
 }
