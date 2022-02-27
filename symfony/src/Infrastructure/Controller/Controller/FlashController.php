4fcdd671d8fc16eed1f1ad3cdd1c219836e58391
malverdo <malverdo11@mail.ru>
2022-02-27 09:27:38 +0300

    FlashController

--- symfony/src/Infrastructure/Controller/Controller/FlashController.php
+++ symfony/src/Infrastructure/Controller/Controller/FlashController.php
@@ -3,8 +3,16 @@
 namespace App\Infrastructure\Controller\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class FlashController extends AbstractController
 {
 
+
+    public function index()
+    {
+        $response = new Response('asd');
+        $this->addFlash('as','asdasd');
+        return $response;
+    }
 }
