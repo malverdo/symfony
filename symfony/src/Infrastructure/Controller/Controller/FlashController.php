f461e849d079859a0819f1e11daad581b2ca2698
malverdo <malverdo11@mail.ru>
2022-02-27 08:24:37 +0300

    add FlashController

---
+++ symfony/src/Infrastructure/Controller/Controller/FlashController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infrastructure\Controller\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FlashController extends AbstractController
+{
+
+}
