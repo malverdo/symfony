55c9f323c83cafd685ee2a0224a3d2dd1f3f7cd7
malverdo <malverdo11@mail.ru>
2022-02-27 11:35:30 +0300

    ref CookieController

--- symfony/src/Infrastructure/Controller/Controller/CookieController.php
+++ symfony/src/Infrastructure/Controller/Controller/CookieController.php
@@ -4,14 +4,17 @@ namespace App\Infrastructure\Controller\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class CookieController extends AbstractController
 {
     public function index(Request $request)
     {
 
+        $contentsDir = $this->getParameter('app.service.test').'/contents';
         $response = new Response('dog');
         $cookie = Cookie::create('foo2')
             ->withExpires(time() + 20)
