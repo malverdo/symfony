e63cff608844ec11c6176562f303c80c2fcf6c90
malverdo <malverdo11@mail.ru>
2022-02-28 07:51:22 +0300

    add onKernelControllerArguments

--- symfony/src/Infrastructure/Controller/Request/RequestController.php
+++ symfony/src/Infrastructure/Controller/Request/RequestController.php
@@ -2,7 +2,18 @@
 
 namespace App\Infrastructure\Controller\Request;
 
-class RequestController
-{
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestController extends AbstractController
+{
+    public function index(Request $request, $a = 1, $b = 3): Response
+    {
+
+        $response = new Response('dog' . $a . ' ' . $b);
+        return $response;
+    }
 }
---
+++ symfony/src/Infrastructure/EventListener/ControllerArguments.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Infrastructure\EventListener;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+
+class ControllerArguments
+{
+
+    public function onKernelControllerArguments(ControllerArgumentsEvent $event)
+    {
+
+        // get controller and request arguments
+        $namedArguments = $event->getRequest()->attributes->all();
+        $controllerArguments = $event->getArguments();
+
+        // set the controller arguments to modify the original arguments or add new ones
+        $event->setArguments(['request' => $controllerArguments[0], 'b' => 2]);
+    }
+
+}
