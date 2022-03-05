08b1eeb0cdd20a8d2b69ea9c785493a961f6cd23
malverdo <malverdo11@mail.ru>
2022-03-05 09:58:24 +0300

    doctrine

---
+++ symfony/migrations/CardsmilePersonal/Version20220305060509.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220305060509 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'The description of my awesome migration!';
+    }
+
+    public function up(Schema $schema): void
+    {
+
+        $this->addSql('CREATE TABLE "user" (id INT  NOT NULL,
+                                            name VARCHAR(255) DEFAULT NULL,
+                                            role INTEGER DEFAULT NULL,
+                                             PRIMARY KEY(id))');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE user');
+    }
+}
