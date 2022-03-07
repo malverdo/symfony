8b2918f1648c34cce5ee9406634cfdd5d8023f71
malverdo <malverdo11@mail.ru>
2022-03-07 17:12:57 +0300

    ref

---
+++ symfony/src/Repository/CategoryRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CardsmilePersonal\Category;
+use App\Infrastructure\Base\AbstractBaseRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class CategoryRepository extends  AbstractBaseRepository
+{
+    public function getModel(): string
+    {
+        return Category::class;
+    }
+
+    public function getBD(): string
+    {
+        return 'cardsmile_personal';
+    }
+
+    public function getTableAlias(): string
+    {
+        return 'c';
+    }
+
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($this->getBD(), $this->getModel(), $registry);
+    }
+
+    public function save(Category $customer)
+    {
+        $this->managerRegistry->persist($customer);
+        $this->managerRegistry->flush();
+    }
+
+    public function update()
+    {
+        $this->managerRegistry->flush();
+    }
+
+    public function findName($name)
+    {
+        $createQueryBuilder = $this->createQueryBuilder($this->getTableAlias())
+            ->where('c.name = :name')
+            ->setParameter('name', $name)
+            ->setFirstResult(0)
+            ->setMaxResults(3);
+
+        $query = $createQueryBuilder->getQuery();
+        return $query->getResult(2);
+
+    }
+}
