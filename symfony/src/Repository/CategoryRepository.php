5038f4d39694a6ae9b13769a3e9ac97f51633188
malverdo <malverdo11@mail.ru>
2022-03-09 08:27:48 +0300

    ref

--- symfony/src/Repository/CategoryRepository.php
+++ symfony/src/Repository/CategoryRepository.php
@@ -35,6 +35,11 @@ class CategoryRepository extends  AbstractBaseRepository
         $this->managerRegistry->flush();
     }
 
+    public  function saveFlush()
+    {
+        $this->managerRegistry->flush();
+    }
+
     public function update()
     {
         $this->managerRegistry->flush();
