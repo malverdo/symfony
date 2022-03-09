064811a7883e66f06150df8eba66774821485c21
malverdo <malverdo11@mail.ru>
2022-03-09 09:04:35 +0300

    SessionController

--- symfony/src/Infrastructure/Controller/Controller/SessionController.php
+++ symfony/src/Infrastructure/Controller/Controller/SessionController.php
@@ -2,7 +2,23 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
-class SessionController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 
+class SessionController extends AbstractController
+{
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function index()
+    {
+//        $this->requestStack->getSession()->set('sd',['asd']);
+        $a = $this->requestStack->getSession()->get('sd');
+       dd($this->requestStack->getSession(), $a);
+    }
 }
