67b1b7409ffaf5017748226898ca66efcadc312b
malverdo <malverdo11@mail.ru>
2022-03-10 08:27:41 +0300

    CacheController

---
+++ symfony/src/Infrastructure/Controller/Controller/CacheController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infrastructure\Controller\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CacheController extends AbstractController
+{
+
+}
