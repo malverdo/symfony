44b7c5f2f140d34412ed7501d42decfdfde88df7
malverdo <malverdo11@mail.ru>
2022-03-10 09:12:50 +0300

    index

--- symfony/src/Infrastructure/Controller/Controller/CacheController.php
+++ symfony/src/Infrastructure/Controller/Controller/CacheController.php
@@ -2,13 +2,30 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
+
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class CacheController extends AbstractController
 {
 
+    private FilesystemAdapter $filesystemAdapter;
+
+    public function __construct()
+    {
+        $this->filesystemAdapter = new FilesystemAdapter();
+    }
+
     public function index()
     {
-        dd('hello world');
+        $value = $this->filesystemAdapter->get('my_cache_key', function (ItemInterface $item) {
+            $item->expiresAfter(20);
+            $computedValue = 'foobar';
+
+            return $computedValue;
+        });
+
+        dd($value);
     }
 }
