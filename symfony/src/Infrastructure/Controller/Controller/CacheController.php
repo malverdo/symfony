b8d0d9455b81a78da957e117a902822d3e4b5932
malverdo <malverdo11@mail.ru>
2022-03-11 09:35:20 +0300

    cache

--- symfony/src/Infrastructure/Controller/Controller/CacheController.php
+++ symfony/src/Infrastructure/Controller/Controller/CacheController.php
@@ -19,13 +19,6 @@ class CacheController extends AbstractController
 
     public function index()
     {
-        $value = $this->filesystemAdapter->get('my_cache_key', function (ItemInterface $item) {
-            $item->expiresAfter(20);
-            $computedValue = 'foobar';
 
-            return $computedValue;
-        });
-
-        dd($value);
     }
 }
