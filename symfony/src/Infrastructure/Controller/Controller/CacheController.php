45f1e923d8c6b372fc0cdb57354d8a06ca308b23
malverdo <malverdo11@mail.ru>
2022-03-14 08:59:39 +0300

    ref

--- symfony/src/Infrastructure/Controller/Controller/CacheController.php
+++ symfony/src/Infrastructure/Controller/Controller/CacheController.php
@@ -5,23 +5,31 @@ namespace App\Infrastructure\Controller\Controller;
 
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Contracts\Cache\ItemInterface;
 
 class CacheController extends AbstractController
 {
 
-    private FilesystemAdapter $filesystemAdapter;
+//    private FilesystemAdapter $filesystemAdapter;
+
+    private RedisAdapter $redisAdapter;
 
     public function __construct()
     {
-        $this->filesystemAdapter = new FilesystemAdapter();
+        $client = RedisAdapter::createConnection(
+            'redis://172.17.0.1'
+        );
+
+//        $this->filesystemAdapter = new FilesystemAdapter('', 0, '/tmp/test');
+        $this->redisAdapter = new RedisAdapter($client, '23');
     }
 
     public function index()
     {
 
-        $value = $this->filesystemAdapter->get('my_cache_key', function (ItemInterface $item) {
-            $item->expiresAfter(20);
+        $value = $this->redisAdapter->get('my_cache_key', function (ItemInterface $item) {
+            $item->expiresAfter(200);
             $computedValue = 'foobar';
 
             return $computedValue;
