a18a1534f788a19737bb466f6bc8cb49f1ef8df7
malverdo <malverdo11@mail.ru>
2022-03-14 09:06:30 +0300

    ref Response

--- symfony/src/Infrastructure/Controller/Router/CacheHttpController.php
+++ symfony/src/Infrastructure/Controller/Router/CacheHttpController.php
@@ -13,7 +13,7 @@ class CacheHttpController  extends AbstractController
     {
 
         $response = new Response(
-            "<html><body>Lucky number: " . 31 . "</body></html>"
+            "<html><body> Lucky number: " . 31 . "</body></html>"
         );
         $response->setPublic();
         // установка tag след запрос если tag такой же верни 304
