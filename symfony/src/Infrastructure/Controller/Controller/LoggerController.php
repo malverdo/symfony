7961e366f98fc31f80646f13996bab1471bc168c
malverdo <malverdo11@mail.ru>
2022-03-16 09:28:29 +0300

    add LoggerController

--- symfony/src/Infrastructure/Controller/Controller/LoggerController.php
+++ symfony/src/Infrastructure/Controller/Controller/LoggerController.php
@@ -2,11 +2,21 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
 class LoggerController
 {
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->info('I just got the logger');
+        $logger->error('An error occurred');
+        $logger->log(LogLevel::INFO, 'An error occurred');
 
+        $logger->critical('I left the oven on!', [
+            // include extra "context" info in your logs
+            'cause' => 'in_hurry',
+        ]);
         dd('hello world');
     }
 }
