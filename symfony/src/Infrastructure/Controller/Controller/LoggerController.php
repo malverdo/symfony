2bd98cb663a050926642fa644719df186e130fa0
malverdo <malverdo11@mail.ru>
2022-03-16 18:42:47 +0300

    GoogleTranslateForFree

--- symfony/src/Infrastructure/Controller/Controller/LoggerController.php
+++ symfony/src/Infrastructure/Controller/Controller/LoggerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
+use Dejurin\GoogleTranslateForFree;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
@@ -9,6 +10,17 @@ class LoggerController
 {
     public function index(LoggerInterface $logger)
     {
+
+        $source = 'en';
+        $target = 'ru';
+        $attempts = 5;
+        $text = 'Hello world';
+
+        $tr = new GoogleTranslateForFree();
+        $result = $tr->translate($source, $target, $text, $attempts);
+
+        dd($result);
+
         $logger->info('I just got the logger');
         $logger->error('An error occurred',);
         $logger->log(LogLevel::INFO, 'An error occurred');
