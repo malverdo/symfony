6ccb6efa4a596c66955c2a13265ae72816a8141a
malverdo <malverdo11@mail.ru>
2022-03-19 17:59:54 +0300

    composer

--- symfony/src/Infrastructure/EventListener/ExceptionListener.php
+++ symfony/src/Infrastructure/EventListener/ExceptionListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\EventListener;
 
+use http\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
@@ -12,26 +13,26 @@ class ExceptionListener
     {
 
         $exception = $event->getThrowable();
-        $message = sprintf(
-            'My Error says: %s with code: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
+        if (is_a($exception,'InvalidArgumentException')) {
+            $message = 'привет';
+            $array = ['Error' => $message];
+            // Customize your response object to display the exception details
+            $response = new Response();
+            $response->setContent(json_encode($array, JSON_UNESCAPED_UNICODE));
 
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent($message);
+            // HttpExceptionInterface is a special type of exception that
+            // holds status code and header details
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
 
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            // sends the modified response object to the event
+            $event->setResponse($response);
         }
-
-        // sends the modified response object to the event
-        $event->setResponse($response);
     }
 }
