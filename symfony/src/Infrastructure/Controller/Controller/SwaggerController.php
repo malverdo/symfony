11d5854dccd728e250df81ac7bf4aca088e4ca47
malverdo <malverdo11@mail.ru>
2022-03-26 11:16:53 +0300

    Swagger

--- symfony/src/Infrastructure/Controller/Controller/SwaggerController.php
+++ symfony/src/Infrastructure/Controller/Controller/SwaggerController.php
@@ -2,11 +2,22 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 
+
+/**
+ * @OA\Info(title="My First API", version="0.1")
+ */
 class SwaggerController
 {
-    public function index()
+    /**
+     * @OA\Get(
+     *     path="/api/resource.json",
+     *     @OA\Response(response="200", description="An example resource")
+     * )
+     */
+    public function index(): Response
     {
         return new Response('dog');
     }
