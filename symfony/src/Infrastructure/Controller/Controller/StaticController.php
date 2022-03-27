cf481247cf191d73e21d313d6768967013c73c4c
malverdo <malverdo11@mail.ru>
2022-03-27 17:42:27 +0300

    static

--- symfony/src/Infrastructure/Controller/Controller/StaticController.php
+++ symfony/src/Infrastructure/Controller/Controller/StaticController.php
@@ -5,4 +5,20 @@ namespace App\Infrastructure\Controller\Controller;
 class StaticController
 {
 
+
+    public function index()
+    {
+        $this->testStatic();
+        $this->testStatic();
+        $this->testStatic();
+        $this->testStatic();
+        dd('2');
+    }
+
+    public function testStatic()
+    {
+        static $a = 0;
+        echo $a;
+        $a++;
+    }
 }
