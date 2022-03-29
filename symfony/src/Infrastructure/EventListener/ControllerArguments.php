8a1580933b9bb167965d64268a4364e79dee7835
malverdo <malverdo11@mail.ru>
2022-03-29 16:35:03 +0300

    ref

--- symfony/src/Infrastructure/EventListener/ControllerArguments.php
+++ symfony/src/Infrastructure/EventListener/ControllerArguments.php
@@ -10,12 +10,9 @@ class ControllerArguments
 
     public function onKernelControllerArguments(ControllerArgumentsEvent $event)
     {
-
         // get controller and request arguments
         $namedArguments = $event->getRequest()->attributes->all();
         $controllerArguments = $event->getArguments();
-
-
         // set the controller arguments to modify the original arguments or add new ones
         $event->setArguments($controllerArguments);
     }
