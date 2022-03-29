8a274c4f7122819188f811c5b49b6b2e1ecfeb22
malverdo <malverdo11@mail.ru>
2022-03-29 16:35:10 +0300

    ref

--- symfony/src/Infrastructure/Fixtures/UserFixture.php
+++ symfony/src/Infrastructure/Fixtures/UserFixture.php
@@ -11,12 +11,10 @@ class UserFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // create 20 products! Bam!
         for ($i = 0; $i < 20000; $i++) {
             $category = Customer::create('malverdo');
             $manager->persist($category);
         }
-
         $manager->flush();
     }
 }
