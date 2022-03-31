b040026503b5319f65165adfec18adb32650e5a3
malverdo <malverdo11@mail.ru>
2022-03-31 11:30:59 +0300

    conroller

--- symfony/src/Infrastructure/Controller/Controller/CallController.php
+++ symfony/src/Infrastructure/Controller/Controller/CallController.php
@@ -2,7 +2,15 @@
 
 namespace App\Infrastructure\Controller\Controller;
 
+use Symfony\Contracts\Cache\ItemInterface;
+
 class CallController
 {
+    public function index()
+    {
 
+        $a = $this->redisAdapter->getItem('my_cache_key23');
+//        $a->set('234');
+        dd($value, $a);
+    }
 }
