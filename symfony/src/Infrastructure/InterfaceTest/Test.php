36367421e006432adf23ac76a385a222f6f18b6b
malverdo <malverdo11@mail.ru>
2022-04-04 14:57:43 +0300

    test

---
+++ symfony/src/Infrastructure/InterfaceTest/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Infrastructure\InterfaceTest;
+
+class Test implements TestThreeInterface
+{
+    public function testOne()
+    {
+        // TODO: Implement testOne() method.
+    }
+
+    public function testTwo()
+    {
+        // TODO: Implement testTwo() method.
+    }
+}
