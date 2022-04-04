80724c5a82bbe56b6aa2316c014b8003a4dd00c2
malverdo <malverdo11@mail.ru>
2022-04-04 09:10:39 +0300

    command

--- symfony/src/Infrastructure/Console/InputTestOneCommand.php
+++ symfony/src/Infrastructure/Console/InputTestOneCommand.php
@@ -2,7 +2,17 @@
 
 namespace App\Infrastructure\Console;
 
+use Symfony\Component\Console\Input\InputArgument;
+
 class InputTestOneCommand
 {
-
+    protected function configure()
+    {
+        $this
+            ->setName('user:rot')
+            ->setDescription('Create a test user.')
+            ->addArgument('email', InputArgument::REQUIRED)
+            ->addArgument('password', InputArgument::REQUIRED)
+            ->addArgument('name', InputArgument::OPTIONAL);
+    }
 }
