5b785d32554a5b64af9029b316b4628c4d434532
malverdo <malverdo11@mail.ru>
2022-04-04 09:04:26 +0300

    add this

--- symfony/src/Infrastructure/Console/InputTestCommand.php
+++ symfony/src/Infrastructure/Console/InputTestCommand.php
@@ -4,5 +4,13 @@ namespace App\Infrastructure\Console;
 
 class InputTestCommand
 {
-
+    protected function configure()
+    {
+        $this
+            ->setName('user:rot')
+            ->setDescription('Create a test user.')
+            ->addArgument('email', InputArgument::REQUIRED)
+            ->addArgument('password', InputArgument::REQUIRED)
+            ->addArgument('name', InputArgument::OPTIONAL);
+    }
 }
