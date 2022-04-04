e446fc56344ebc2a56205ab1aa1f9f7693030a67
malverdo <malverdo11@mail.ru>
2022-04-04 09:04:38 +0300

    add this

--- symfony/src/Infrastructure/Console/InputTestCommand.php
+++ symfony/src/Infrastructure/Console/InputTestCommand.php
@@ -13,4 +13,16 @@ class InputTestCommand
             ->addArgument('password', InputArgument::REQUIRED)
             ->addArgument('name', InputArgument::OPTIONAL);
     }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $name = $input->getArgument('name');
+
+        $output->writeln(
+            sprintf('Электронный адрес - %s, Пароль - %s, Имя - %s', $email, $password, $name ?? 'default')
+        );
+        return 1;
+    }
 }
