91ad415845138f484a04cca1c47bc4968aea1c4f
malverdo <malverdo11@mail.ru>
2022-04-10 13:32:41 +0300

    many to many

---
+++ symfony/src/Entity/Cardsmile/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity\Cardsmile;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\Table;
+
+
+/**
+ * @Entity(repositoryClass="App\Repository\UserRepository")
+ * @Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $name;
+
+    /**
+     * Many Users have Many Groups.
+     * @ManyToMany(targetEntity="Group", inversedBy="users")
+     * @JoinTable(name="users_groups")
+     */
+    private $groups;
+
+
+
+    public function __construct() {
+        $this->groups = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getGroups(): \Doctrine\Common\Collections\ArrayCollection
+    {
+        return $this->groups;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
