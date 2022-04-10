56eab104f3386ce704b3cde39e155ca0280fa12c
malverdo <malverdo11@mail.ru>
2022-04-10 13:33:02 +0300

    many to many

---
+++ symfony/src/Repository/UserRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cardsmile\Category;
+use App\Entity\Cardsmile\User;
+use App\Infrastructure\Base\AbstractBaseRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserRepository extends  AbstractBaseRepository
+{
+    public function getModel(): string
+    {
+        return User::class;
+    }
+
+    public function getBD(): string
+    {
+        return 'cardsmile';
+    }
+
+    public function getTableAlias(): string
+    {
+        return 'c';
+    }
+
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($this->getBD(), $this->getModel(), $registry);
+    }
+
+    public function save(User $customer)
+    {
+        $this->managerRegistry->persist($customer);
+        $this->managerRegistry->flush();
+    }
+
+    public  function saveFlush()
+    {
+        $this->managerRegistry->flush();
+    }
+
+    public function update()
+    {
+        $this->managerRegistry->flush();
+    }
+
+    public function findName($name)
+    {
+        $createQueryBuilder = $this->createQueryBuilder($this->getTableAlias())
+            ->where('c.name = :name')
+            ->setParameter('name', $name)
+            ->setFirstResult(0)
+            ->setMaxResults(3);
+
+        $query = $createQueryBuilder->getQuery();
+        return $query->getResult(2);
+
+    }
+}
